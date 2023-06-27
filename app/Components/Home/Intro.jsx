76574dd1d290b3102ad8e136a9ecff2f08d63ad8
tbrown034 <tbrown034@gmail.com>
2023-06-27 01:52:26 -0500

    WIP(Intro): Design and responsivensnss

--- app/Components/Home/Intro.jsx
+++ app/Components/Home/Intro.jsx
@@ -11,66 +11,67 @@ import { motion } from "framer-motion";
 
 export default function Intro() {
   return (
-    <section className="flex flex-col justify-between min-h-screen px-4 py-8">
-      <div className="">
-        <motion.h1
-          className="text-6xl font-montserrat sm:text-8xl"
-          initial={{ y: -100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
-          Hello👋, I'm Trevor
-        </motion.h1>
-        <motion.h3
+    <section className="flex flex-col min-h-screen px-4 pt-24">
+      <div className="sm:grid-cols-6 sm:grid">
+        <div className="col-span-3">
+          <motion.h1
+            className="text-6xl font-montserrat"
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            Hello👋, I'm Trevor
+          </motion.h1>
+
+          <motion.h3
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="text-2xl font-bold leading-relaxed font-robotoSlab"
+          >
+            I'm a{" "}
+            <span className="inline px-1 py-0.5 text-xl underline dark:bg-blue-600 bg-blue-300 rounded shadow-md font-firaCode">
+              full-stack web developer
+            </span>{" "}
+            with a knack for{" "}
+            <span className="inline px-1 py-0.5 text-xl  dark:bg-blue-600 underline bg-blue-300 rounded shadow-md font-firaCode">
+              data-driven
+            </span>{" "}
+            programming and storytelling. After more than a decade as an{" "}
+            <span className="inline px-1 py-0.5 text-xl underline  dark:bg-blue-600 bg-blue-300 rounded shadow-md font-firaCode">
+              award-winning investigative journalist
+            </span>
+            , I'm now looking for new opportunities in the world of tech. Let's
+            build something cool.
+          </motion.h3>
+        </div>
+
+        <motion.div
           initial={{ x: -100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 1 }}
-          className="text-2xl font-bold leading-relaxed font-robotoSlab"
+          className="col-span-3 "
         >
-          I'm a{" "}
-          <span className="inline px-1 py-0.5 text-xl underline dark:bg-blue-600 bg-blue-300 rounded shadow-md font-firaCode">
-            full-stack web developer
-          </span>{" "}
-          with a knack for{" "}
-          <span className="inline px-1 py-0.5 text-xl  dark:bg-blue-600 underline bg-blue-300 rounded shadow-md font-firaCode">
-            data-driven
-          </span>{" "}
-          programming and storytelling. After more than a decade as an{" "}
-          <span className="inline px-1 py-0.5 text-xl underline  dark:bg-blue-600 bg-blue-300 rounded shadow-md font-firaCode">
-            award-winning investigative journalist
-          </span>
-          , I'm now looking for new opportunities in the world of tech. Let's
-          build something cool.
-        </motion.h3>
-      </div>
-      <div>
-        <motion.div
-          initial={{ scale: 0 }}
-          animate={{ rotate: 360, scale: 1 }}
-          transition={{
-            delay: 1,
-            type: "spring",
-            stiffness: 260,
-            damping: 20,
-          }}
-          className="w-1/2 mx-auto "
-        >
-          <Image className=" rounded-3xl" src={photo} alt="Trevor Brown" />
+          <Image
+            className="w-4/6 rounded-3xl sm:flex"
+            src={photo}
+            alt="Trevor Brown"
+          />
         </motion.div>
+      </div>
 
-        <div className="flex justify-center gap-4">
-          <button className="p-2 border-2 border-blue-800 rounded-lg hover:bg-blue-300 active:bg-blue-400">
-            About
-          </button>
+      <div className="flex justify-center gap-4">
+        <button className="p-2 border-2 border-blue-800 rounded-lg hover:bg-blue-300 active:bg-blue-400">
+          About
+        </button>
 
-          <button className="p-2 border-2 border-blue-800 rounded-lg hover:bg-blue-300 active:bg-blue-400">
-            Resume
-          </button>
+        <button className="p-2 border-2 border-blue-800 rounded-lg hover:bg-blue-300 active:bg-blue-400">
+          Resume
+        </button>
 
-          <button className="p-2 border-2 border-blue-800 rounded-lg hover:bg-blue-300 active:bg-blue-400">
-            Contact
-          </button>
-        </div>
+        <button className="p-2 border-2 border-blue-800 rounded-lg hover:bg-blue-300 active:bg-blue-400">
+          Contact
+        </button>
       </div>
       <div className="flex justify-between">
         <Link href="/" className="flex items-center space-x-2">
--- app/Components/UI/header.jsx
+++ app/Components/UI/header.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import Link from "next/link";
 import { CodeBracketIcon } from "@heroicons/react/24/outline";
 import { NewspaperIcon } from "@heroicons/react/24/outline";
@@ -20,27 +20,33 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between p-2 text-2xl dark:text-blue-200 dark:bg-blue-800">
       <h1
-        className="font-majorMono"
+        className="font-majorMono sm:hidden"
         style={{ textShadow: "rgba(0, 0, 0, 0.5) 1px 0px 0px" }}
       >
         &lt;TrevorBrown /&gt;
       </h1>
+      <h1
+        className="hidden sm:flex font-majorMono"
+        style={{ textShadow: "rgba(0, 0, 0, 0.5) 1px 0px 0px" }}
+      >
+        &lt;TB /&gt;
+      </h1>
 
-      <div className="hidden gap-8 sm:flex opacity-80">
+      <div className="hidden gap-8 text-base lg:text-2xl sm:flex opacity-80">
         <Link href="/">
-          <div className="flex gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400">
+          <div className="flex items-center gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400">
             <CodeBracketIcon className="w-6 h-6" />
             <p>Coding</p>
           </div>
         </Link>
         <Link href="/">
-          <div className="flex gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400">
+          <div className="flex items-center gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400">
             <NewspaperIcon className="w-6 h-6" />
             <p>Journalism</p>
           </div>
         </Link>
         <Link href="/">
-          <div className="flex gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400">
+          <div className="flex items-center gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400">
             <ChartBarIcon className="w-6 h-6" />
             <p>Data Viz</p>
           </div>
