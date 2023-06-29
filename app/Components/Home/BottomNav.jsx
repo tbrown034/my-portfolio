1825f8aea6ec1636db777873688482314894f7a6
tbrown034 <tbrown034@gmail.com>
2023-06-28 21:18:29 -0500

    feat(home): added hr with border above the bottomnav

--- app/Components/Home/BottomNav.jsx
+++ app/Components/Home/BottomNav.jsx
@@ -6,10 +6,9 @@ import { UserIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 export default function BottomNav() {
   return (
-    <nav className="text-sm rounded-lg p lg:text-2xl sm:text-lg">
-      <div className="bg-white  text-blue-800 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2 self-start">
-        <p>Discover More</p>
-      </div>
+    <nav className="text-sm lg:text-2xl sm:text-lg">
+      <hr className="w-2/3 mx-auto border-t-2 border-blue-800 dark:border-blue-200" />
+
       <div className="flex justify-between">
         <Link href="#coding">
           <div className="flex items-center px-4 py-2 space-x-3 transition-all duration-200 rounded-lg cursor-pointer hover:bg-blue-200 hover:shadow-lg">
