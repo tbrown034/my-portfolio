2c9a8fc3e6a25051de0655515d0b6e825e2f7741
tbrown034 <tbrown034@gmail.com>
2023-07-05 23:38:10 -0500

    feat(code): even better formatting, spacing

--- app/Components/Coding/techStack.jsx
+++ app/Components/Coding/techStack.jsx
@@ -9,7 +9,7 @@ import {
 
 export default function techStack() {
   return (
-    <section className="flex flex-col gap-8">
+    <section className="flex flex-col gap-4">
       <h1 className="text-2xl font-semibold underline font-montserrat ">
         Tech Stack and Skills{" "}
       </h1>
@@ -36,6 +36,26 @@ export default function techStack() {
             Tailwind CSS.
           </span>
         </p>
+        <div className="flex flex-wrap justify-center gap-4 py-4">
+          <div className="">
+            <SiJavascript className="" size={50} />
+          </div>
+          <div className="">
+            <SiNodedotjs size={50} />
+          </div>
+          <div className="">
+            <SiReact size={50} />
+          </div>
+          <div className="">
+            <SiNextdotjs size={50} />
+          </div>
+          <div className="">
+            <SiTailwindcss size={50} />
+          </div>
+          <div className="">
+            <SiPostgresql size={50} />
+          </div>
+        </div>
         <p className="text-xl font-robotoSlab">
           I also have experience using JS libraries that includes{" "}
           <span className="p-1 text-sm bg-blue-800 rounded-lg text-blue-50 ">
@@ -58,26 +78,6 @@ export default function techStack() {
             Axios.
           </span>
         </p>
-      </div>
-      <div className="flex flex-wrap justify-center gap-4 py-4">
-        <div className="">
-          <SiJavascript className="" size={50} />
-        </div>
-        <div className="">
-          <SiNodedotjs size={50} />
-        </div>
-        <div className="">
-          <SiReact size={50} />
-        </div>
-        <div className="">
-          <SiNextdotjs size={50} />
-        </div>
-        <div className="">
-          <SiTailwindcss size={50} />
-        </div>
-        <div className="">
-          <SiPostgresql size={50} />
-        </div>
       </div>
     </section>
   );
