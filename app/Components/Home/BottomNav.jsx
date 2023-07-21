7f12aafe1a0dcd0ecb13065566e58c03debf4a9c
tbrown034 <tbrown034@gmail.com>
2023-07-20 23:23:41 -0500

    fix(Home): made bottom nav consistent

--- app/Components/Home/BottomNav.jsx
+++ app/Components/Home/BottomNav.jsx
@@ -10,23 +10,35 @@ export default function BottomNav() {
       <hr className="w-2/3 p-4 mx-auto border-t-2 border-blue-800 dark:border-blue-200" />
 
       <div className="flex flex-wrap justify-around gap-4">
-        <a href="#coding" className="flex items-center gap-1">
+        <a
+          href="#coding"
+          className="flex items-center gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400"
+        >
           <CodeBracketIcon className="w-4 sm:w-6 lg:w-8" />
           <span className="">Coding</span>
           <ChevronDoubleDownIcon className="w-4 h-4" />
         </a>
 
-        <a href="#journalism" className="flex items-center gap-1">
+        <a
+          href="#journalism"
+          className="flex items-center gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400"
+        >
           <NewspaperIcon className="w-4 sm:w-6 lg:w-8" />
           <span className="">Journalism</span>
           <ChevronDoubleDownIcon className="w-4 h-4" />
         </a>
-        <a href="#dataviz" className="flex items-center gap-1">
+        <a
+          href="#dataviz"
+          className="flex items-center gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400"
+        >
           <ChartBarIcon className="w-4 sm:w-6 lg:w-8" />
           <span className="">Data Visualizations</span>
           <ChevronDoubleDownIcon className="w-4 h-4" />
         </a>
-        <Link href="#about" className="flex items-center gap-1">
+        <Link
+          href="#about"
+          className="flex items-center gap-1 p-1 rounded-lg cursor-pointer hover:bg-blue-300 active:bg-blue-400"
+        >
           <UserIcon className="w-4 sm:w-6 lg:w-8" />
           <span className="">About</span>
           <ChevronDoubleDownIcon className="w-4 h-4" />
