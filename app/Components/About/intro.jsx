caa159a5f9a5e5d948ce52db89cd785a23f96b48
tbrown034 <tbrown034@gmail.com>
2023-08-17 17:07:08 -0400

    another cache reset

--- app/Components/About/intro.jsx
+++ app/Components/About/intro.jsx
@@ -5,7 +5,7 @@ export default function Intro() {
     <section className="flex flex-col gap-4">
       <div className="bg-blue-100 text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-blue-300  dark:text-blue-800  self-start gap-2">
         <UserIcon className="w-6 h-6 font-bold" />
-        <p className="text-lg">Contact </p>
+        <p className="text-lg">Contact</p>
       </div>
 
       <h2 className="text-3xl font-bold lg:text-center font-montserrat sm:text-4xl">
